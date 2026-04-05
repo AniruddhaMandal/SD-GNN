bc2ec07b30484d994d5ffe8437710c9f802c4b5c
Aniruddha Mandal <ani96dh@gmail.com>
2026-04-06 01:01:43 +0530

    06-04-26 night

---
+++ delme.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+cd /home/ani/code/SD-GNN
+source venvSD/bin/activate
+
+# ZINC ablations
+gxl -c configs/arch_10/ZINC/ablations/no_inter_conv.json
+gxl -c configs/arch_10/ZINC/ablations/no_bfs_pe.json
+gxl -c configs/arch_10/ZINC/ablations/no_logp_pe.json
+gxl -c configs/arch_10/ZINC/ablations/mean_pool_readout.json
+gxl -c configs/arch_10/ZINC/ablations/no_inter_no_pe.json
+
+# MolHIV HT ablations
+gxl -c configs/arch_24/OGB/molhiv_ht_inter.json
+gxl -c configs/arch_24/OGB/molhiv_ht_pool.json
+
+# Multi-seed ZINC
+gxl -c configs/arch_24/ZINC/m32.json --multi-seed --seeds 42 43 44 45
